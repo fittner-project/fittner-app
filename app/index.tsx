5ccb39fba4ab0129fb5ab7b806d93ca15f39e398
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-10 19:26:26 +0900

    [CREATE] 안드로이드 뒤로가기 대응

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,21 @@
+import useHardwareBack from "@/hooks/useHardwareBack";
+import { useRef } from "react";
 import { WebView } from "react-native-webview";
 
+const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+
 export default function Index() {
-  return <WebView source={{ uri: "https://www.naver.com" }} />;
+  const webViewRef = useRef<WebView>(null);
+  const handleNavigationStateChange = useHardwareBack(webViewRef);
+
+  return (
+    <WebView
+      style={{ flex: 1 }}
+      textZoom={100}
+      injectedJavaScript={INJECTEDJAVASCRIPT}
+      onNavigationStateChange={handleNavigationStateChange}
+      ref={webViewRef}
+      source={{ uri: "https://m.fittner.co.kr/" }}
+    />
+  );
 }
---
+++ hooks/useHardwareBack.ts
@@ -0,0 +1,55 @@
+import { RefObject, useEffect, useState } from "react";
+import { BackHandler, ToastAndroid } from "react-native";
+import WebView from "react-native-webview";
+
+type WebViewNavigation = {
+  url: string;
+  title: string;
+  loading: boolean;
+  canGoBack: boolean;
+  canGoForward: boolean;
+};
+
+export default function useHardwareBack(webViewRef: RefObject<WebView<{}>>) {
+  const [currentUrl, setCurrentUrl] = useState("");
+  const [lastBackPressed, setLastBackPressed] = useState(0);
+
+  const onPressHardwareBackButton = () => {
+    const now = Date.now();
+    if (currentUrl === "https://m.fittner.co.kr/") {
+      if (now - lastBackPressed <= 2000) {
+        BackHandler.exitApp();
+      } else {
+        ToastAndroid.show(
+          "뒤로가기 버튼을 한번 더 누르면 종료됩니다.",
+          ToastAndroid.SHORT
+        );
+        setLastBackPressed(now);
+      }
+      return true;
+    } else if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener(
+      "hardwareBackPress",
+      onPressHardwareBackButton
+    );
+    return () => {
+      BackHandler.removeEventListener(
+        "hardwareBackPress",
+        onPressHardwareBackButton
+      );
+    };
+  }, [currentUrl, lastBackPressed]);
+
+  const handleNavigationStateChange = (navState: WebViewNavigation) => {
+    setCurrentUrl(navState.url);
+  };
+
+  return handleNavigationStateChange;
+}
